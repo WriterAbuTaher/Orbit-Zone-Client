58ba3546f839a5e73a03d8b7d523d29b9065e8a0
Abu Taher <writerabutaher@gmail.com>
2022-11-25 03:52:29 +0600

    sponsors section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Feature from './Feature';
 import Hero from './Hero';
+import Sponsors from './Sponsors';
 
 const Home = () => {
     return (
         <>
             <Hero></Hero>
             <Feature></Feature>
+            <Sponsors></Sponsors>
         </>
     );
 };
