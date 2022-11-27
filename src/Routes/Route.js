9ff6ae361f0449f5056d1ae554b5e4adf936fc46
Abu Taher <writerabutaher@gmail.com>
2022-11-27 12:30:33 +0600

    blog added

--- src/Routes/Route.js
+++ src/Routes/Route.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Blog from "../Pages/Blog/Blog";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
@@ -16,6 +17,10 @@ export const router = createBrowserRouter([
             {
                 path: '/home',
                 element: <Home></Home>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
             },
             {
                 path: '/login',
--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -36,10 +36,10 @@ const Navbar = () => {
                                 <div className="block w-full h-full md:h-auto">
                                     <ul className="space-y-8 tracking-wide font-medium md:flex md:space-y-0">
                                         <li>
-                                            <NavLink to='/home' className="block md:px-3">
-                                                <div className="relative text-yellow-800 dark:text-yellow-300
-                                                    before:absolute before:-inset-2 before:w-full before:h-0.5 before:origin-left dark:before:bg-yellow-500 before:mx-auto before:mt-auto before:rounded-full before:bg-yellow-800">
-                                                    <span>Home</span>
+                                            <NavLink to='/home' className="block md:px-3 group">
+                                                <div className="relative text-gray-600
+                                                    before:absolute before:-inset-2 before:w-full before:h-0.5 before:origin-left dark:before:bg-yellow-500 before:mx-auto before:mt-auto before:rounded-full before:bg-yellow-800 before:transition before:scale-x-0 group-hover:before:scale-x-100">
+                                                    <span className="transition group-hover:text-yellow-700 dark:text-gray-300 dark:group-hover:text-yellow-300">Home</span>
                                                 </div>
                                             </NavLink>
                                         </li>
